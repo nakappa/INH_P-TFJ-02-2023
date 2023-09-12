a9870efd681c2ec69b827867a8466f9f90a79e8a
nakappa <nakappa1@hotmail.com>
2023-09-12 14:57:17 -0300

    create HeaderContent.vue

--- to_do_list/src/App.vue
+++ to_do_list/src/App.vue
@@ -1,16 +1,11 @@
 <script setup lang="ts">
-  import FormLogin from './components/molecules/FormLogin.vue';
+  import HeaderContent from './components/HeaderContent.vue';
   import MainContent from './components/MainContent.vue';
 </script>
 
 <template>
   <div class="container">
-    <header class="header">
-      <h1 class="title">Lista de Tarefas</h1>
-
-      <FormLogin />
-    </header>
-
+    <HeaderContent />
     <MainContent />
   </div>
 </template>
@@ -26,54 +21,11 @@
     flex-direction: column;
     align-items: center;
   }
+  
+  .title { font-weight: 700; }
 
   h1.title {
     font-size: var(--size48);
     color: var(--quaternary);
-  }
-
-  header {
-    width: 100%;
-    display: flex;
-    gap: 24px;
-    flex-wrap: wrap;
-    flex-direction: column;
-    align-items: center;
-  }
-
-  header .title { font-weight: 700; }
-
-  header .form-login {
-    width: 210px;
-    display: flex;
-    gap: 12px;
-    flex-direction: column;
-    justify-content: center;
-  }
-  
-  @media (min-width: 800px) {
-    header {
-      gap: 80px;
-      flex-direction: row;
-      justify-content: space-between;
-    }
-
-    header .form-login {
-      width: min(600px, 100%);
-      flex-direction: row;
-      align-items: center;
-    }
-
-    header .form-login .field {
-      min-width: 210px;
-      flex-direction: row;
-      align-items: center;
-    }
-
-    header .form-login .btn { height: 48px; }
-  }
-
-  @media (min-width: 800px) and (max-width: 1085px) {
-    header { justify-content: center; }
   }
 </style>
---
+++ to_do_list/src/components/HeaderContent.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+	import FormLogin from './molecules/FormLogin.vue';
+</script>
+
+<template>
+	<header class="header">
+		<h1 class="title">Lista de Tarefas</h1>
+		<FormLogin />
+	</header>
+</template>
+
+<style scoped>
+  header {
+    width: 100%;
+    display: flex;
+    gap: 24px;
+    flex-wrap: wrap;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  @media (min-width: 800px) {
+    header {
+      gap: 80px;
+      flex-direction: row;
+      justify-content: space-between;
+    }
+  }
+
+  @media (min-width: 800px) and (max-width: 1085px) {
+    header { justify-content: center; }
+  }
+</style>
