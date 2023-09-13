9cbff4a043262733d7a057a885f848c9efca9b70
nakappa <nakappa1@hotmail.com>
2023-09-13 11:09:28 -0300

    Create BtnLogout.vue

---
+++ to_do_list/src/components/atomics/buttons/BtnLogout.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+  import type { User } from '@/types/Users';
+
+  defineProps<{ user:User }>();
+</script>
+
+<template>
+  <button type="button" class="btn logout" @click="console.log('sair')">
+    sair
+    <span class="material-symbols-outlined">logout</span>
+  </button>
+</template>
+
+<style scoped>
+  .logout {
+    width: 140px !important;
+    gap: 10px;
+    width: 100%;
+    height: var(--size48);
+    border-radius: 180px;
+    font-size: 16px;
+    font-weight: 700;
+    text-transform: uppercase;
+    transition: .75s all;
+    background-color: var(--quaternary);
+  }
+
+  .logout:hover {
+    box-shadow: 0 0 5px var(--quaternary),
+                0 0 10px var(--quaternary),
+                0 0 15px var(--quaternary),
+                0 0 20px var(--quaternary);
+  }
+</style>
