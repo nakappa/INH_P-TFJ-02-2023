a36e9235b8a271088dad294c0f2a5c9e1fc6fa65
nakappa <nakappa1@hotmail.com>
2023-09-13 11:09:51 -0300

    Create UserLogged.vue

---
+++ to_do_list/src/components/molecules/UserLogged.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+	import type { User } from '@/types/Users';
+  import BtnLogout from '../atomics/buttons/BtnLogout.vue';
+
+  defineProps<{ user: User }>();
+</script>
+
+<template>
+	<div class="user">
+		<h2 class="user-name">Usuários: <span>{{ user.name }}</span></h2>
+		<BtnLogout :user="user" />
+	</div>
+</template>
+
+<style scoped>
+  .user {
+    width: 100%;
+    display: flex;
+    gap: 12px;
+		flex-wrap: wrap;
+		align-items: center;
+		justify-content: center;
+  }
+
+	.user-name {
+		width: 100%;
+		height: 48px;
+		margin: 0;
+		display: flex;
+		gap: 10px;
+		align-items: center;
+		justify-content: center;
+		text-align: center;
+		font-size: var(--size24);
+	}
+	
+	.user-name span {
+		font-weight: 700;
+		color: var(--quaternary);
+		text-transform: uppercase;
+	}
+
+	@media (min-width: 515px) {
+		.user {
+			width: max-content;
+			flex-wrap: nowrap;
+		}
+	}
+</style>
