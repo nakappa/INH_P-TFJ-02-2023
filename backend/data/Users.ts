2c870a9460bf45bf7b3df53367597f5bdc870ba0
Inhangá <nakappa1@hotmail.com>
2023-09-15 18:57:08 -0300

    steps project

--- backend/data/Users.ts
+++ backend/data/Users.ts
@@ -1,24 +1,3 @@
 import { User } from '../types/User';
 
-export const users: Array<User> = [
-	{
-		id: 0,
-		name: 'Inhangá Silveira',
-		cpf: 29109875077,
-		password: '123123',
-		tasks: [
-			{
-				id: 0,
-				act: true,
-				description: 'Testando o projeto',
-				title: 'Teste'
-			},
-			{
-				id: 1,
-				act: false,
-				description: 'Testando',
-				title: 'Tarefa'
-			},
-		]
-	}
-];
+export const users: Array<User> = [];
